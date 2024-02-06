362b2bcd7806ce111fdc01c0e05419a069e1ccd6
PLAyEr2002002 <95416181+PLAyEr2002002@users.noreply.github.com>
2024-02-06 21:10:49 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Weather Forecast
  Weather forecast with API calls and streamlit hosting
+
+
+# Weather Forecast Application
+
+This application provides weather forecasts for the next few days for a specified location. It leverages Streamlit for an interactive web interface and fetches forecast data from the OpenWeatherMap API.
+
+## Features
+
+- **Location Input**: Users can enter a location to receive a weather forecast.
+- **Forecast Period Selection**: Users can select the number of forecast days, up to a maximum of 5 days.
+- **Data Visualization**: Displays temperature forecasts and sky conditions using Plotly for temperature data and static images for sky conditions.
+
+## Installation
+
+To run this project, ensure Python is installed on your system. Follow these steps:
+
+1. **Clone the repository or download the project files.**
+2. **Navigate to the project directory.**
+3. **Install the required dependencies:**
+
+```bash
+pip install streamlit plotly requests
---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st 
+import plotly.express as px
+from backend import get_data
+
+
+st.title("Weather Forecast for the next Days")
+place =st.text_input("Place: ")
+days= st.slider("Forecast Days", min_value=1, max_value=5, 
+                help="Select the number of forecasted days")
+
+option= st.selectbox("Select data to view", ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
+
+
+
+if place:
+    
+    try:
+        
+        filtered_data= get_data(place, days)
+        if option=="Temperature":
+            temperatures= [value["main"]["temp"] /10 for value in filtered_data]
+            # temperatures= [temp-273.15 for temp in temperatures]
+            dates=[value["dt_txt"] for value in filtered_data]
+            figure= px.line(x=dates, y=temperatures, labels={"x":"Date", "y":"Temperature(c)"})
+            st.plotly_chart(figure)
+
+
+        if option=="Sky":
+            images={"Clear":"images/clear.png","Clouds":"images/cloud.png","Rain":"images/rain.png","Snow":"images/snow.png"}
+            sky_condition= [value["weather"][0]["main"] for value in filtered_data]   
+            image_paths= [images[condition] for condition in sky_condition]
+
+            st.image(image_paths,width=115,caption=sky_condition)
+            
+    except KeyError:
+        st.header(f"No data found   ~ {place} ~   Does not exist :)")
